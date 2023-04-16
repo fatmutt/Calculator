545a6af79d266fe8c1602044b72e3254978fc078
fatmutt <danieljsinger9@gmail.com>
2023-04-15 18:11:07 -0700

    initial commit

---
+++ calc.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    const sum = a + b;
+    return sum;
+};
+
+function subtract(a, b) {
+    const sum = a - b;
+    return sum;
+}
+
+function multiply(a,b) {
+   return a*b;
+};
+
+function divide(a,b) {
+    return a/b;
+}
+
+let x = 0;
+let y = 0;
+let operator = 0;
+
+
+
+function operate() {
+
+}
