a7476ea174bce8776f01e675a0d1f73963e78b27
fatmutt <danieljsinger9@gmail.com>
2023-05-07 21:05:43 -0700

    new push

--- calc.js
+++ calc.js
@@ -1,3 +1,36 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+}
+
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const plus = document.getElementById('+');
+
+const minus = document.getElementById('-');
+
+const times = document.getElementById('x');
+
+const division = document.getElementById('÷');
+
+let digits = Array.from(document.getElementsByClassName('digit'));
+
+let operators = Array.from(document.getElementsByClassName('oper'));
+
+let c = document.getElementById('clear');
+
+let d = document.getElementById('delete');
+
+let equals = document.getElementById('=');
+
+let period = document.getElementById('.');
+
+let displayInput = document.getElementById('currentOperand');
+
 function add(a, b) {
     const sum = a + b;
     return sum;
@@ -6,7 +39,7 @@ function add(a, b) {
 function subtract(a, b) {
     const sum = a - b;
     return sum;
-}
+};
 
 function multiply(a,b) {
    return a*b;
@@ -14,14 +47,59 @@ function multiply(a,b) {
 
 function divide(a,b) {
     return a/b;
-}
-
-let x = 0;
-let y = 0;
-let operator = 0;
+};
 
 
 
-function operate() {
+function operate(a, operator, b) {
+a = Number(a);
+b = Number(b);
+    switch(operator) {
+        case '+':
+            return add(a,b)
+        case '-':
+            return subtract(a,b)
+        case 'x':
+            return multiply(a,b)
+        case '÷':
+            if (b === 0) return null
+            else return divide(a,b)
+        default:
+            return null
+    };
+};
 
-}
+equals.addEventListener('click', () => {
+
+})
+
+
+
+digits.map( digit => {
+    digit.addEventListener('click', (e) => {
+        displayInput.innerText += e.target.innerText;
+    });
+});
+
+operators.map( oper => {
+    oper.addEventListener('click', () => {
+        if (oper)
+        
+        
+    });
+});
+
+c.addEventListener('click', () => {
+    this.currentOperand;
+    this.previousOperand;
+    this.operation = undefined
+
+});
+
+d.addEventListener('click', () => {
+    displayInput.innerText = displayInput.innerText.slice(0, -1);
+});
+
+equals.addEventListener('click', () => {
+calculate();
+});
